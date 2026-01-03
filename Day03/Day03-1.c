5e57212f785fa1013be21696c5c08ae181698a71
Inexpert_trifler <saranshh.yadav@gmail.com>
2026-01-03 10:51:21 +0530

    day 6

---
+++ Day03/Day03-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int toSeconds(int hours, int minutes, int seconds){
+    return hours*60*60 + minutes*60 + seconds;
+}
+
+int main() {
+    int hours,minutes,seconds;
+    scanf("%d %d %d",&hours,&minutes,&seconds);
+    int sec=toSeconds(hours,minutes,seconds);
+    printf("Total seconds: %d",sec);
+
+    
+    return 0;
+}
+//Convert Time into Seconds
