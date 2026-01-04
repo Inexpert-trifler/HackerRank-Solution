0b09824d5c8dbdd77fb33ad36c883ce15b5d69e6
Inexpert_trifler <saranshh.yadav@gmail.com>
2026-01-04 11:37:37 +0530

    day 4 solution

---
+++ Day04/Day04-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+void split(int numb){
+    printf("%d %d %d", (numb/100), ((numb/10)%10), (numb%10));
+}
+
+int main() {
+    
+    int num;
+    scanf("%d", &num);
+    split(num);
+    return 0;
+}
+//Split the Number
+
---
+++ Day04/Day04-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+        
+    printf("Half of %d is %.2f", num, (num/2.00));    
+    return 0;
+}
+//Half the Number
