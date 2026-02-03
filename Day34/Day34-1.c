8af9cfbbb02a348fb86bcb44ccefcbead861267b
Inexpert_trifler <saranshh.yadav@gmail.com>
2026-02-03 23:45:43 +0530

    Day 34 code

---
+++ Day34/Day34-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long t = 1;
+
+    scanf("%d", &n);
+
+    if (n == 0)
+        return 0;
+
+    printf("<");
+
+    for (int i = 1; i <= n; i++) {
+        printf("%lld", t);
+        if (i < n)
+            printf(" ");
+        t *= 2;
+    }
+
+    printf(">");
+
+    return 0;
+}
+//Loops_Double the Previous Term
