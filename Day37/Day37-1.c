9b2582ce8c7bde8c1179fb8b8969ddd79aca3bd2
Inexpert_trifler <saranshh.yadav@gmail.com>
2026-02-06 13:28:47 +0530

    day 37 code

---
+++ Day37/Day37-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n,a,d;
+    scanf("%d",&n);
+    scanf("%d %d",&a,&d);
+    for(int i=0; i<n ; i++ ){
+        printf("%d ",a + i * d);
+    }
+
+    
+    
+    
+    return 0;
+}
+
+//Loops_Arithmetic Progression
