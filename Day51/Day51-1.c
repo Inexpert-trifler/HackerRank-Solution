d7989bd3381a889ddaf8c2636a365c99f0def22d
Inexpert_trifler <saranshh.yadav@gmail.com>
2026-02-19 08:40:45 +0530

    day51 code

---
+++ Day51/Day51-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int n, sum = 0, p = 1;
+
+    scanf("%d", &n);
+
+    for (int t = n; t > 0; t /= 10) {
+        int digit = t % 10;
+        sum += digit;
+        p *= digit;
+    }
+
+    printf("%d", p - sum);
+
+    return 0;
+}
+//Loops_Subtract the Product and Sum of Digits of an Integer
