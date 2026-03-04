26690ea7463b5d598e7fa401f505e0ef9d1ff823
Inexpert_trifler <saranshh.yadav@gmail.com>
2026-03-04 22:05:51 +0530

    day60

---
+++ Day60/Day60-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    int sum = 0;   
+    int x;
+    
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+    
+        scanf("%d", &x);
+        sum += x;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
